2fc155182f4257ed54cc58449cc99613f6926821
Rijnard van Tonder <rvantonder@gmail.com>
2020-07-24 02:06:12 -0700

    add -f for only files

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,10 +17,11 @@ fi
 if [ -f "comby-files-64a2a202-cd8b-11ea-87d0-0242ac130003.txt" ]; then
   echo "see files.txt!"
   COMBY_PR_FILES=$(cat comby-files-64a2a202-cd8b-11ea-87d0-0242ac130003.txt)
+  ONLY_FILES="-f $COMBY_PR_FILES"
 fi
 
 # replace in place
-/usr/local/bin/comby -config .comby -i "$COMBY_PR_FILES"
+/usr/local/bin/comby -config .comby -i $ONLY_FILES
 # get the patch
 git diff > p.patch
 # debug
