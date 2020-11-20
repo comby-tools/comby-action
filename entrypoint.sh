d43d6caf17bbe57e713118f47b548da6b156b357
Rijnard van Tonder <rvantonder@gmail.com>
2020-11-20 04:43:35 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,27 +2,15 @@
 
 set -x
 
-cd $GITHUB_WORKSPACE
+cd $GITHUB_WORKSPACE # Needed?
 
-if [ -z "$COMBY_CONFIG" ]; then
-    if [ ! -f .comby ]; then
-        echo "No configuration file. Expected .comby in current directory, or config option set in action."
-        exit 1
-    fi
-    COMBY_CONFIG=".comby"
+/usr/local/bin/comby $*
+git diff &> p.patch
+if [ -s p.patch ]; then
+    # non-zero => empty
+    echo ::set-output name=exit-code::0
 else
-    curl $COMBY_CONFIG > .comby
+    # zero => has content, emit 2.
+    echo ::set-output name=exit-code::2
 fi
-
-if [ -f "comby-files-64a2a202-cd8b-11ea-87d0-0242ac130003.txt" ]; then
-  echo "see files.txt!"
-  COMBY_PR_FILES=$(cat comby-files-64a2a202-cd8b-11ea-87d0-0242ac130003.txt)
-  ONLY_FILES="-f $COMBY_PR_FILES"
-fi
-
-# replace in place
-/usr/local/bin/comby -config .comby -i $ONLY_FILES
-# get the patch
-git diff > p.patch
-# debug
-cat p.patch
+rm p.patch
