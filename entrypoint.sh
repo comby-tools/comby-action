6feea7fee581e3d08f3eda19ded31fc28f5b96fc
Rijnard van Tonder <rvantonder@gmail.com>
2020-11-20 04:45:22 -0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,8 @@ cd $GITHUB_WORKSPACE # Needed?
 
 /usr/local/bin/comby $*
 git diff &> p.patch
-if [ -s p.patch ]; then
-    # non-zero => empty
-    echo ::set-output name=exit-code::0
-else
-    # zero => has content, emit 2.
-    echo ::set-output name=exit-code::2
-fi
+[ -s p.patch ]
+# non-zero => empty, no diff
+# zero => there is a diff
+echo $?
 rm p.patch
